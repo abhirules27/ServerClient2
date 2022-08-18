7dac1dce9f7f84fb6d74551c7fa0ac2d6011ae27
abhirules27 <abhirules27@gmail.com>
2022-08-18 20:56:55 +0530

    Client Coded

--- Client2.py
+++ Client2.py
@@ -1,1 +1,14 @@
 
+import socket
+HEADERSIZE = 10
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))
+while True:
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f"New msg length: {msg[:HEADERSIZE]}")
+        full_msg += msg.decode('utf-8')
+    print(full_msg)
