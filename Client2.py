1f8ed23ed091c20dca6e49ec237e9eebf211fe35
abhirules27 <abhirules27@gmail.com>
2022-08-19 13:11:52 +0530

    Client Code

--- Client2.py
+++ Client2.py
@@ -1,21 +1,24 @@
 import socket
 HEADERSIZE = 10
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# IP_SERVER = '192.168.0.194'
-IP_SERVER = '5.180.61.49'
+IP_SERVER = '192.168.0.199'
+# IP_SERVER = '5.180.61.49'
 s.connect((IP_SERVER, 1234))
+
 while True:
     full_msg = ''
     new_msg = True
     while True:
         msg = s.recv(16)
         if new_msg:
-            print(f"New msg length: {msg[:HEADERSIZE]}")
+            print(msg)
+            print(f"\n New msg length: {msg[:HEADERSIZE]}\n ")
             msglen = int(msg[:HEADERSIZE])
             new_msg = False
         full_msg += msg.decode('utf-8')
+
         if len(full_msg) - HEADERSIZE == msglen:
-            print("Full Message Received")
+            print("Full Message Received\n")
             print(full_msg[HEADERSIZE:])
             new_msg = True
-    print(full_msg)
+        print(full_msg)
